f883d3656cc9d726a4a1d3ea7701b8b3ae755d3e
myself659 <myself659@gmail.com>
2019-08-14 23:22:04 +0800

    quickcheck

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,47 @@
 use std::cmp::Ordering;
+use std::iter::{FromIterator, IntoIterator};
+use itertools::equal;
 
 type AvlTree<T> = Option<Box<AvlNode<T>>>;
 
 #[derive(Debug, PartialEq, Clone)]
-struct AvlNode<T:Ord> {
-    value: T, 
+struct AvlNode<T: Ord> {
+    value: T,
     left: AvlTree<T>,
     right: AvlTree<T>,
 }
 
-
-
-#[derive(Debug, PartialEq,Clone)]
-struct AvlTreeSet<T:Ord> {
+#[derive(Debug, PartialEq, Clone)]
+struct AvlTreeSet<T: Ord> {
     root: AvlTree<T>,
 }
 
-impl<T:Ord>  AvlTreeSet<T>{
-    fn new() ->Self{
-        Self{ root: None}
+impl<T: Ord> AvlTreeSet<T> {
+    fn new() -> Self {
+        Self { root: None }
     }
 
-    fn insert(&mut self, value:T) -> bool{
+    fn insert(&mut self, value: T) -> bool {
         let mut current_tree = &mut self.root;
-        while let Some(current_node) = current_tree{
-            match current_node.value.cmp(&value){
-                Ordering::Less=>{
+        while let Some(current_node) = current_tree {
+            match current_node.value.cmp(&value) {
+                Ordering::Less => {
                     current_tree = &mut current_node.right;
                 }
-                Ordering::Equal =>{
-                    return  false;
+                Ordering::Equal => {
+                    return false;
                 }
-                Ordering::Greater =>{
-                    current_tree = &mut  current_node.left;
+                Ordering::Greater => {
+                    current_tree = &mut current_node.left;
                 }
             }
         }
 
-        *current_tree = Some(Box::new(AvlNode{
+        *current_tree = Some(Box::new(AvlNode {
             value,
-            left:None,
-            right:None,})); 
+            left: None,
+            right: None,
+        }));
         true
     }
 }
@@ -55,41 +56,41 @@ impl<'a, T: 'a + Ord> Iterator for AvlTreeSetIter<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-    loop {
-        match *self.current_tree {
-            None => match self.prev_nodes.pop() {
-                None => {
-                    return None;
-                }
+        loop {
+            match *self.current_tree {
+                None => match self.prev_nodes.pop() {
+                    None => {
+                        return None;
+                    }
 
-                Some(ref prev_node) => {
-                    self.current_tree = &prev_node.right;
+                    Some(ref prev_node) => {
+                        self.current_tree = &prev_node.right;
 
-                    return Some(&prev_node.value);
-                }
-            },
+                        return Some(&prev_node.value);
+                    }
+                },
 
-            Some(ref current_node) => {
-                if current_node.left.is_some() {
-                    self.prev_nodes.push(&current_node);
-                    self.current_tree = &current_node.left;
+                Some(ref current_node) => {
+                    if current_node.left.is_some() {
+                        self.prev_nodes.push(&current_node);
+                        self.current_tree = &current_node.left;
 
-                    continue;
-                }
+                        continue;
+                    }
 
-                if current_node.right.is_some() {
-                    self.current_tree = &current_node.right;
+                    if current_node.right.is_some() {
+                        self.current_tree = &current_node.right;
+
+                        return Some(&current_node.value);
+                    }
+
+                    self.current_tree = &None;
 
                     return Some(&current_node.value);
                 }
-
-                self.current_tree = &None;
-
-                return Some(&current_node.value);
             }
         }
     }
-}
 }
 
 // Addition of lifetime parameter for the set
@@ -99,6 +100,18 @@ impl<'a, T: 'a + Ord> AvlTreeSet<T> {
             prev_nodes: Vec::new(),
             current_tree: &self.root,
         }
+    }
+}
+
+impl<T: Ord> FromIterator<T> for AvlTreeSet<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut set = Self::new();
+
+        for i in iter {
+            set.insert(i);
+        }
+
+        set
     }
 }
 
@@ -117,3 +130,34 @@ fn main() {
     assert_eq!(iter.next(), None);
 }
 
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+#[cfg(test)]
+mod properties {
+    use super::*;
+    use std::collections::BTreeSet;
+
+    // 1. Create a random list
+    #[quickcheck]
+    fn iterator_parity(mut xs: Vec<usize>) -> bool {
+        // 2. Create an empty AVL tree and BTree
+        // 3. For each element in the list, insert it to both tree
+        let avl_set = xs.iter().cloned().collect::<AvlTreeSet<_>>();
+        let btree_set = xs.iter().cloned().collect::<BTreeSet<_>>();
+
+        // 4. Both AVL and BTree iterator should be equal item per item
+        equal(avl_set.iter(), btree_set.iter())
+       
+        //true
+    }
+    #[quickcheck]
+    fn insert_parity(mut btree_set: BTreeSet<u8>, x: u8) -> bool {
+        let mut avl_set = btree_set.iter().cloned().collect::<AvlTreeSet<_>>();
+
+        avl_set.insert(x) == btree_set.insert(x)
+    }
+}
