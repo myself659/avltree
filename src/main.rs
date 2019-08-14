0cfe7743a9bcfd294475d73e0cf6a0bbe3056aa6
myself659 <myself659@gmail.com>
2019-08-14 22:21:37 +0800

    iter

--- src/main.rs
+++ src/main.rs
@@ -45,25 +45,75 @@ impl<T:Ord>  AvlTreeSet<T>{
     }
 }
 
+#[derive(Debug)]
+struct AvlTreeSetIter<'a, T: Ord> {
+    prev_nodes: Vec<&'a AvlNode<T>>,
+    current_tree: &'a AvlTree<T>,
+}
+
+impl<'a, T: 'a + Ord> Iterator for AvlTreeSetIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+    loop {
+        match *self.current_tree {
+            None => match self.prev_nodes.pop() {
+                None => {
+                    return None;
+                }
+
+                Some(ref prev_node) => {
+                    self.current_tree = &prev_node.right;
+
+                    return Some(&prev_node.value);
+                }
+            },
+
+            Some(ref current_node) => {
+                if current_node.left.is_some() {
+                    self.prev_nodes.push(&current_node);
+                    self.current_tree = &current_node.left;
+
+                    continue;
+                }
+
+                if current_node.right.is_some() {
+                    self.current_tree = &current_node.right;
+
+                    return Some(&current_node.value);
+                }
+
+                self.current_tree = &None;
+
+                return Some(&current_node.value);
+            }
+        }
+    }
+}
+}
+
+// Addition of lifetime parameter for the set
+impl<'a, T: 'a + Ord> AvlTreeSet<T> {
+    fn iter(&'a self) -> AvlTreeSetIter<'a, T> {
+        AvlTreeSetIter {
+            prev_nodes: Vec::new(),
+            current_tree: &self.root,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     let mut set = AvlTreeSet::new();
 
-assert!(set.insert(1));   // Insert new value
-assert!(!set.insert(1));  // Should not insert existing value
+    for i in (1..4 as usize).rev() {
+        set.insert(i);
+    }
 
-assert!(set.insert(2));   // Insert another new value
-assert_eq!(               // Checking the tree structure
-    set.root,
-    Some(Box::new(AvlNode {
-        value: 1,
-        left: None,
-        right: Some(Box::new(AvlNode {
-            value: 2,
-            left: None,
-            right: None
-        })),
-    }))
-);
+    let mut iter = set.iter();
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), None);
 }
 
